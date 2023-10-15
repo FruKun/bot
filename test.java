033de77757e35ea89e73c10380787916f5b1b63c
FruKun <127653755+FruKun@users.noreply.github.com>
2023-10-15 21:47:22 +0500

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args){
-        String a = "abcd";
-        for(String c : a.split("")){
-            System.out.println(c);
-        }
-        
-    }
-}
