3a518df7b96d136d69caeba3b002a1a19bd4f105
FruKun <peromata@yandex.ru>
2023-10-15 21:45:47 +0500

    1

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        String a = "abcd";
+        for(String c : a.split("")){
+            System.out.println(c);
+        }
+        
+    }
+}
