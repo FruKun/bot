06ed5d8b282b1b96f9e0e85334f5832a5ab7c1f3
FruKun <peromata@yandex.ru>
2023-10-15 21:36:13 +0500

    gitignore

--- my.java
+++
@@ -1,9 +0,0 @@
-public class my {
-    public static void main(String[] args){
-        String a = "abcd";
-        for(String c : a.split("")){
-            System.out.println(c);
-        }
-        
-    }
-}
